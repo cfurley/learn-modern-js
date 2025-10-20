89b3cedf4a231c1fa04bd8d2163dd6b18ff95547
caleb furley <caleb.furley@gmail.com>
2025-10-20 15:12:32 -0500

    Worked on ternary operator and basic mathematics.

---
+++ src/basic_math.js
@@ -0,0 +1,7 @@
+let x = 5;
+let y = 7;
+const result = x + y;
+
+this.console.log(result);
+this.console.log(typeof result);
+this.document.body.innerHTML = "x + y = " + result;
---
+++ src/ternary_operator.js
@@ -0,0 +1,6 @@
+// example of the ternary operator.
+// T/F ? true-code : else-code;
+
+let x = 10;
+
+x > 9 ? console.log("greater than") : console.log("less than");
